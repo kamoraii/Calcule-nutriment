9c742eda78b9a7bc551da6862eeac03c4d3b2a17
kamoraii <ninovu@hotmail.com>
2026-05-10 02:14:04 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "nutrition-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./aliments.js",
+  "./manifest.json",
+  "./favicon.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
